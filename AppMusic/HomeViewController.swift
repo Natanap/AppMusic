0dbbacf22aeae2c726ecff64749d5453fe36eaf5
Natanael Alves <natanalvespr@gmail.com>
2023-01-08 20:21:47 -0300

    First commit

---
+++ AppMusic/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  AppMusic
+//
+//  Created by Natanael Alves Pereira on 08/01/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
