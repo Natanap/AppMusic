6cb3c8c624c99bf6a90f8e861974b120831278ef
Natanael Alves <natanalvespr@gmail.com>
2023-02-03 14:58:24 -0300

    Create detailview

--- AppMusic/Detail/DetailView/DetailView.swift
+++ AppMusic/Detail/DetailView/DetailView.swift
@@ -66,4 +66,37 @@ class DetailView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setUpView() {
+        self.addSubview(self.scrollView)
+        self.scrollView.addSubview(cardView)
+        self.scrollView.addSubview(self.tableView)
+        self.addSubview(self.closeBtn)
+    }
+    
+    private func setUpConstraints() {
+        let window = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .compactMap ({$0 as? UIWindowScene})
+            .first?.windows.filter({$0.isKeyWindow}).first
+        
+        let topPading = window?.safeAreaInsets.top
+        print(topPading)
+        
+        self.scrollView.pin(to: self)
+        
+        NSLayoutConstraint.activate([
+            self.cardView.topAnchor.constraint(equalTo: self.scrollView.topAnchor,constant: -(topPading ?? 0)),
+            self.cardView.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor),
+            self.cardView.heightAnchor.constraint(equalToConstant: 500),
+            self.cardView.widthAnchor.constraint(equalToConstant: self.frame.size.width),
+        
+            self.tableView.topAnchor.constraint(equalTo: self.cardView.topAnchor),
+            self.tableView.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor),
+            self.tableView.heightAnchor.constraint(equalToConstant: CGFloat((80 * (cardModel?.cardList?.count ?? 0)) + 20)),
+            self.tableView.widthAnchor.constraint(equalToConstant: self.frame.size.width),
+            self.tableView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+            
+        ])
+    }
 }
