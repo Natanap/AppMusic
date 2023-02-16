23f3f865313a948ebde3de03bec4634c705f8194
Natanael Alves <natanalvespr@gmail.com>
2023-02-16 11:50:52 -0300

    Add constraints

--- AppMusic/CustomView/CustomNavBar.swift
+++ AppMusic/CustomView/CustomNavBar.swift
@@ -52,10 +52,47 @@ class CustomNavBar: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.setupViews()
+        self.setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupViews() {
+        self.addSubview(self.cardImage)
+        self.addSubview(self.overlayView)
+        self.addSubview(self.categoryLabel)
+        self.addSubview(self.cardTitle)
+        self.addSubview(self.featureLabel)
+    }
+    
+    private func setupConstraints() {
+        
+        self.cardImage.pin(to: self)
+        self.overlayView.pin(to: self)
+        
+        NSLayoutConstraint.activate([
+            self.categoryLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 5),
+            self.categoryLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            self.categoryLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            
+            self.cardTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 20),
+            self.cardTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -20),
+            self.cardTitle.topAnchor.constraint(equalTo: self.categoryLabel.bottomAnchor, constant: 5),
+            
+            self.featureLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            self.featureLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            self.featureLabel.topAnchor.constraint(equalTo: self.cardTitle.bottomAnchor, constant: 5),
+            
+        ])
+    }
+    
+    public func setupView(data: CardViewModel) {
+        self.cardTitle.text = data.cardTitle
+        self.categoryLabel.text = data.categoryName
+        self.cardImage.image = UIImage(named: data.cardImage ?? "")
+        self.featureLabel.attributedText = .featureText(data.likeCount ?? "", data.duration ?? "")
+    }
 }
